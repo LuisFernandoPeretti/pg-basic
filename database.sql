0b1ab0a297712bf2a7458e0649813bb9dc9ec614
Luis Fernando <nando.lfp@hotmail.com>
2022-01-17 14:35:20 -0300

    final

--- database.sql
+++ database.sql
@@ -1,8 +1,23 @@
-CREATE DATABASE pgregister;
-
---\c into pgregister database
+CREATE DATABASE pgRegister;
 
 CREATE TABLE register(
-    email VARCHAR PRIMARY KEY,
-    name VARCHAR(255),
-)
+    email VARCHAR(255) PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    cpf BIGINT NOT NULL,
+    birth_date DATE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE OR REPLACE FUNCTION update_changetimestamp_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_register_changetimestamp BEFORE UPDATE
+    ON register FOR EACH ROW EXECUTE PROCEDURE 
+    update_changetimestamp_column();
--- index.js
+++ index.js
@@ -1,6 +1,85 @@
 const express = require("express");
 const app = express();
+const pool = require("./db");
 
-app.listen(3000, () => {
+app.use(express.json()) // -> req. body
+
+//routes
+
+// get all registers
+
+app.get("/registers", async (req, res) => {
+    try {
+        const allRegisters = await pool.query("SELECT * FROM register")
+
+        res.json(allRegisters.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// get a registers
+
+app.get("/registers/:email", async (req, res) => {
+    const { email } = req.params;
+    try {
+        const register = await pool.query("SELECT * FROM register WHERE email = $1", [email]);
+        res.json(register.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// create a register
+
+app.post("/registers", async (req, res) => {
+    try {
+        const { email, name, surname, cpf, birth_date } = req.body
+        const newRegister = await pool.query(
+            "INSERT INTO register ( email, name, surname, cpf, birth_date ) VALUES ( $1, $2, $3, $4, $5 ) RETURNING *",
+            [ email, name, surname, cpf, birth_date ]
+        );
+
+        res.json(newRegister.rows[0]);
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+// update a register
+
+app.put("/registers/:email", async (req, res) => {
+    try {
+        const { email } = req.params;
+        const { name } = req.body;
+        const { surname } = req.body;
+        const { cpf } = req.body;
+        const { birth_date } = req.body;
+        const { created_at } = req.body;
+        const { updated_at } = req.body;
+
+        //console.log(email);
+        //console.log(name);
+        const updateRegister = await pool.query("UPDATE register SET name = $1, surname = $2, cpf = $3, birth_date = $4, created_at = $5, updated_at = $6 WHERE email = $7", [name, surname, cpf, birth_date, created_at, updated_at, email]);
+
+        res.json("updated!");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// delete a register
+
+app.delete("/registers/:email", async (req, res) => {
+    try {
+        const { email } = req.params;
+        const deleteRegister = await pool.query("DELETE FROM register WHERE email = $1" , [email])
+        res.json("Register was successfuuuly deleted!")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.listen(5000, () => {
     console.log("SERVER IS LISTENING ON PORT 3000")
 });
